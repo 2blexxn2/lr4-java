aa459331be1dba1fcaafcf0f77809e8e889986ed
2blexn <300amfkall@gmail.com>
2024-02-25 21:03:11 +0200

    ivan gay

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,44 @@
-abstract class FlyingMachine {
+import java.util.Arrays;
+
+abstract class FlyingMachine implements Comparable<FlyingMachine> {
+    private String model;
+    private int passengerCapacity;
+    private double maxSpeed;
+
+    FlyingMachine(String model, int passengerCapacity, double maxSpeed) {
+        this.model = model;
+        this.passengerCapacity = passengerCapacity;
+        this.maxSpeed = maxSpeed;
+    }
+
     abstract void fly();
+
+    String getModel() {
+        return model;
+    }
+
+    int getPassengerCapacity() {
+        return passengerCapacity;
+    }
+
+    double getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    @Override
+    public int compareTo(FlyingMachine o) {
+        return Double.compare(this.maxSpeed, o.maxSpeed);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        FlyingMachine that = (FlyingMachine) obj;
+        return passengerCapacity == that.passengerCapacity &&
+                Double.compare(that.maxSpeed, maxSpeed) == 0 &&
+                model.equals(that.model);
+    }
 }
 
 interface Flyable {
@@ -8,79 +47,89 @@ interface Flyable {
     }
 }
 
-class Airplane extends FlyingMachine implements Flyable, Comparable<Airplane>, Cloneable {
+class Airplane extends FlyingMachine implements Flyable, Cloneable {
+    Airplane(String model, int passengerCapacity, double maxSpeed) {
+        super(model, passengerCapacity, maxSpeed);
+    }
+
     @Override
     void fly() {
         System.out.println("Airplane is flying...");
     }
 
     @Override
-    public int compareTo(Airplane o) {
-        return 0;
-    }
-
-    @Override
     protected Object clone() throws CloneNotSupportedException {
         return super.clone();
     }
 
     @Override
     public String toString() {
-        return "This is an Airplane";
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        return obj instanceof Airplane;
+        return "Airplane{" +
+                "model='" + getModel() + '\'' +
+                ", passengerCapacity=" + getPassengerCapacity() +
+                ", maxSpeed=" + getMaxSpeed() +
+                '}';
     }
 }
 
-class Helicopter extends FlyingMachine implements Flyable, Comparable<Helicopter>, Cloneable {
+class Helicopter extends FlyingMachine implements Flyable, Cloneable {
+    Helicopter(String model, int passengerCapacity, double maxSpeed) {
+        super(model, passengerCapacity, maxSpeed);
+    }
+
     @Override
     void fly() {
         System.out.println("Helicopter is flying...");
     }
 
     @Override
-    public int compareTo(Helicopter o) {
-        return 0;
-    }
-
-    @Override
     protected Object clone() throws CloneNotSupportedException {
         return super.clone();
     }
 
     @Override
     public String toString() {
-        return "This is a Helicopter";
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        return obj instanceof Helicopter;
+        return "Helicopter{" +
+                "model='" + getModel() + '\'' +
+                ", passengerCapacity=" + getPassengerCapacity() +
+                ", maxSpeed=" + getMaxSpeed() +
+                '}';
     }
 }
 
 public class Main {
     public static void main(String[] args) throws CloneNotSupportedException {
-        Airplane airplane1 = new Airplane();
+        Airplane airplane1 = new Airplane("Boeing 747", 366, 988);
         Airplane airplane2 = (Airplane) airplane1.clone();
 
-        Helicopter helicopter1 = new Helicopter();
+        Helicopter helicopter1 = new Helicopter("Bell 206", 5, 217);
         Helicopter helicopter2 = (Helicopter) helicopter1.clone();
 
         callFly(airplane1);
         callTakeOff(airplane1);
+        printDetails(airplane1);
 
         callFly(helicopter1);
         callTakeOff(helicopter1);
+        printDetails(helicopter1);
 
         callFly(airplane2);
         callTakeOff(airplane2);
+        printDetails(airplane2);
 
         callFly(helicopter2);
         callTakeOff(helicopter2);
+        printDetails(helicopter2);
+
+        System.out.println("Airplane1 equals Airplane2: " + airplane1.equals(airplane2));
+        System.out.println("Helicopter1 equals Helicopter2: " + helicopter1.equals(helicopter2));
+
+        FlyingMachine[] flyingMachines = {airplane1, airplane2, helicopter1, helicopter2};
+        Arrays.sort(flyingMachines);
+        System.out.println("After sorting by max speed:");
+        for (FlyingMachine flyingMachine : flyingMachines) {
+            printDetails(flyingMachine);
+        }
     }
 
     static void callFly(FlyingMachine flyingMachine) {
@@ -89,5 +138,9 @@ public class Main {
 
     static void callTakeOff(Flyable flyable) {
         flyable.takeOff();
+    }
+
+    static void printDetails(FlyingMachine flyingMachine) {
+        System.out.println(flyingMachine);
     }
 }
